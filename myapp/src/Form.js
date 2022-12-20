b1d232ef1e23abbd2640da4ee70a7e483c8ff44b
Akash <akashsheelavant4@gmail.com>
2022-12-20 18:52:25 +0530

    Deleting console.log statement

--- myapp/src/Form.js
+++ myapp/src/Form.js
@@ -15,8 +15,7 @@ class Form extends Component {
     }
 
     handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(this.state);
+        e.preventDefault();        
     }
 
     render() {
